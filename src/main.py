8ab7410e672517f3d4c852330bef940475e6b9d2
lura00 <ludvig.skinstad@gmail.com>
2022-03-03 10:53:03 +0100

    fixed the conn with postgres-image and created a table and made sure post-requests worked

--- src/main.py
+++ src/main.py
@@ -20,3 +20,14 @@ def create(details: CreateJobRequest, db: Session = Depends(get_db)):
         "created_id": to_create.id
     }
 
+@app.get("/", response_model=CreateJobRequest)
+def get_posts(db: Session = Depends(get_db)):
+
+    posts = db.query(Job).all()
+
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No posts was not found")
+
+    return {"data": posts}
+
--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
 class CreateJobRequest(BaseModel):
+    id: int
     title: str
     description: str
+
+    class config:
+        orm_mode = True
